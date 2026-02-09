f3f5c6150e4706f881b1c462be9ca65e5cbbf46b
Avalons <maikelflorentio@gmail.com>
2026-02-09 20:09:14 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -9,21 +9,23 @@ export default function Testimonials() {
         {["Jorgi", "Alex", "Kevin"].map((name) => (
           <div
             key={name}
-            className="bg-neutral-900 p-6 rounded-xl border border-neutral-800"
+            className="bg-neutral-900 py-6 rounded-xl border border-neutral-800"
           >
-            <div className="flex flex-row items-center pb-4">
+            <div className="flex flex-row items-center pb-4 px-6">
               <img
                 src="/coach.png"
-                className="h-13 w-13 rounded-full mr-2 object-cover border border-[#CC983F]"
+                className="h-13 w-13 rounded-full mr-2 object-cover border border-[#CC983F] "
               />
 
               <h4 className="font-semibold text-white">{name}</h4>
             </div>
 
-            <p className="text-gray-400 text-sm">
+            <p className="text-gray-400 text-sm px-6">
               Gue cuma ikut 2 sesi tapi aim sama positioning langsung naik.
               Coach-nya ngajarin simpel banget.
             </p>
+
+            <img src="/testimony.png" className="h-55 w-full mt-4 rounded-md"/>
           </div>
         ))}
       </div>
